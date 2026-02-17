98715b5b1f2a5ef0265a92a7fd6e47d055b8ef8f
Maksym-Tykhonenko <maksym.tykhonenko88@gmail.com>
2026-02-17 09:58:27 +0200

    change exp date

--- [WildSpheresBattleArena]/SpheresRoutes/WildSpheresStack.js
+++ [WildSpheresBattleArena]/SpheresRoutes/WildSpheresStack.js
@@ -343,7 +343,7 @@ const WildSpheresStack = () => {
     const checkUrl = `${INITIAL_URL}${URL_IDENTIFAIRE}`;
     //console.log('checkUrl==========+>', checkUrl);
 
-    const targetData = new Date('2026-02-13T08:08:00'); //дата з якої поч працювати webView
+    const targetData = new Date('2026-02-19T08:08:00'); //дата з якої поч працювати webView
     const currentData = new Date(); //текущая дата
 
     if (currentData <= targetData) {
